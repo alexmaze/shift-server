ad881cfe0495e9a31060a31fa118b801712e6b2f
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2016-04-21 23:31:59 +0800

    add deploy btn for CodeBox

--- public/scripts/components/workbench/CodeBox.js
+++ public/scripts/components/workbench/CodeBox.js
@@ -19,17 +19,30 @@
             html.push('<div id="', this.id, '" class="CodeBox">');
             html.push('<div class="codeArea">');
             html.push('<pre><code class="javascript"></code></pre>');
+            html.push('<button class="deploy">Deploy</button>')
             html.push('</div>');
             html.push('</div>');
 
             $parent.append(html.join(''));
 
-            this.__el = $parent.find('#' + this.id).find('code');
+            this.__el = $parent.find('#' + this.id);
+            this.__el_code = this.__el.find('code');
+
+            this.bind();
         },
 
         update: function(code) {
-            this.__el.text(code);
-            hljs.highlightBlock(this.__el[0]);
+            this.__el_code.text(code);
+            hljs.highlightBlock(this.__el_code[0]);
+        },
+
+        bind: function() {
+            this.__el.find('.deploy').on('click', function(event) {
+                event.preventDefault();
+                /* Act on the event */
+                console.log('Deploy!');
+
+            });
         }
 
     });
