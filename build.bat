cb58e1071a5ad1aa4200ee92fb65bbafd224e665
unknown <yanhao1991@gmail.com>
2018-07-22 09:28:38 +0800

    fix cross env

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
-set UV4_HOME="C:\Program Files\Keil\UV4"
+set UV4_HOME="C:\Keil\UV4"
+set ARMCC_BIN="C:\Keil\ARM\ARMCC\bin"
 
-del /q .\code\MasterApp\MDK-ARM\Master\capptask*
+set WORK_DIR="C:\Users\Alex\Desktop\cdi\smartnoderYth\Master\MasterApp\MDK-ARM\Master"
 
-%UV4_HOME%\UV4.exe -b .\code\MasterApp\MDK-ARM\Master.uvprojx -j0 -o ".\build.log"
+del /q %WORK_DIR%\capptask*
+del /q  %WORK_DIR%\Master.axf
+del /q  %WORK_DIR%\Master.bin
 
+%UV4_HOME%\UV4.exe -b  %WORK_DIR%\Master.uvprojx -j0 -o ".\build.log"
+:%ARMCC_BIN%\fromelf --bin -o Master.bin  %WORK_DIR%/Master.axf
